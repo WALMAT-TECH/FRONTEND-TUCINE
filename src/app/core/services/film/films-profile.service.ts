1acf91f77bc6c314d8c9faedc9fe76dd407d9291
Axel Fiestas <josue123452002@gmail.com>
2023-09-22 23:24:43 -0500

    New URL´s comment

--- src/app/core/services/film/films-profile.service.ts
+++ src/app/core/services/film/films-profile.service.ts
@@ -18,7 +18,7 @@ export class FilmsProfileService {
 
   updateMovieProfile(id: number, data: any): Observable<any>{
 
-    //http://localhost:8080/api/TuCine/v1/films
+    //http://localhost:8080/api/TuCine/v1/films/${id}
     return this._http.put(`https://backend-tucine-production.up.railway.app/api/TuCine/v1/films/${id}`,data); //Falta implementar
   }
 
@@ -30,68 +30,68 @@ export class FilmsProfileService {
 
   getMoviebyId(id: number): Observable<any>{
 
-    //http://localhost:8080/api/TuCine/v1/films
+    //http://localhost:8080/api/TuCine/v1/films/${id}
     return this._http.get(`https://backend-tucine-production.up.railway.app/api/TuCine/v1/films/${id}`);
   }
 
   deleteMovieProfile(id: number): Observable<any>{
 
-    //http://localhost:8080/api/TuCine/v1/films
-    return this._http.delete(`http://localhost:3000/Film/${id}`) //Falta implementar
+    //http://localhost:8080/api/TuCine/v1/films/${id}
+    return this._http.delete(`http://localhost:3000/Film/${id}`)
   }
 
   //Showtimes
   getShowtimesbyFilmId(id: number): Observable<any>{
 
-    //http://localhost:8080/api/TuCine/v1/films
+    http://localhost:8080/api/TuCine/v1/films/${id}/showtimes
     return this._http.get(`https://backend-tucine-production.up.railway.app/api/TuCine/v1/films/${id}/showtimes`);
   }
 
   getBusinessById(id: number): Observable<any> {
 
-    //http://localhost:8080/api/TuCine/v1/films
+    //http://localhost:8080/api/TuCine/v1/businesses/${id}
     return this._http.get(`https://backend-tucine-production.up.railway.app/api/TuCine/v1/businesses/${id}`);
   }
 
   getBusinessTypeById(id: number): Observable<any> {
 
-    //http://localhost:8080/api/TuCine/v1/films
+    //http://localhost:8080/api/TuCine/v1/businessTypes/${id}
     return this._http.get(`https://backend-tucine-production.up.railway.app/api/TuCine/v1/businessTypes/${id}`);
   }
 
   getBusiness(): Observable<any[]>{
 
-    //http://localhost:8080/api/TuCine/v1/films
+    //http://localhost:8080/api/TuCine/v1/businesses
     return this._http.get<any[]>('https://backend-tucine-production.up.railway.app/api/TuCine/v1/businesses');
   }
 
   getBusinessType(): Observable<any[]>{
 
-    //http://localhost:8080/api/TuCine/v1/films
+    //http://localhost:8080/api/TuCine/v1/businessTypes
     return this._http.get<any[]>('https://backend-tucine-production.up.railway.app/api/TuCine/v1/businessTypes');
   }
 
   getShowtimebyId(id: number): Observable<any>{
 
-    //http://localhost:8080/api/TuCine/v1/films
+    //http://localhost:8080/api/TuCine/v1/showtimes/${id}
     return this._http.get(`https://backend-tucine-production.up.railway.app/api/TuCine/v1/showtimes/${id}`);
   }
 
   getFilmActorbyFilmId(id: number): Observable<any>{
 
-    //http://localhost:8080/api/TuCine/v1/films
+    //http://localhost:8080/api/TuCine/v1/films/${id}/actors
     return this._http.get(`https://backend-tucine-production.up.railway.app/api/TuCine/v1/films/${id}/actors`);
   }
 
   getActorList(): Observable<any[]>{
 
-    //http://localhost:8080/api/TuCine/v1/films
+    //http://localhost:8080/api/TuCine/v1/actors
     return this._http.get<any[]>('https://backend-tucine-production.up.railway.app/api/TuCine/v1/actors');
   }
 
   getCineclubById(id: number): Observable<any>{
 
-    //http://localhost:8080/api/TuCine/v1/films
+    //http://localhost:8080/api/TuCine/v1/businesses/${id}
     return this._http.get(`https://backend-tucine-production.up.railway.app/api/TuCine/v1/businesses/${id}`);
   }
 
