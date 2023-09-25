e281b3115ca53c1dc05740189382d325e47a1105
Armix18 <armix182002@gmail.com>
2023-09-25 08:43:55 -0500

    Second file modified

--- src/app/core/services/auth/owner/owner-profile.service.ts
+++ src/app/core/services/auth/owner/owner-profile.service.ts
@@ -9,18 +9,15 @@ export class OwnerProfileService {
 
   constructor(private _http: HttpClient) {}
 
-  //http://localhost:8080/api/TuCine/v1/owners
   addOwner(data: any): Observable<any>{
-    return this._http.post('http://localhost:3000/owner',data);
+    return this._http.post('http://localhost:8080/api/TuCine/v1/owners',data);
   }
 
-  //http://localhost:8080/api/TuCine/v1/owners
 /*  updateOwner(id: number, data: any): Observable<any>{
-    return this._http.put(`http://localhost:3000/owner${id}`,data);
+    return this._http.put(`http://localhost:8080/api/TuCine/v1/owners`,data);
   }*/
 
-  //http://localhost:8080/api/TuCine/v1/owners
   getOwnerList(): Observable<any>{
-    return this._http.get('http://localhost:3000/owner');
+    return this._http.get('http://localhost:8080/api/TuCine/v1/owners');
   }
 }
